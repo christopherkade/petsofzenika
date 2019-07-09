a77ac7f988c40831bd41330635e4989062deb8ce
christopherkade <c.kade96@gmail.com>
2019-07-09 13:43:00 +0200

    💄 Update Wrapper styles

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Header } from "components/Header"
 import { AnimalCard } from "components/AnimalCard"
 
 const Wrapper = styled.div`
-  margin-top: 2rem;
+  margin: 2rem 0;
   display: flex;
   flex-wrap: wrap;
   padding: 0;
